fd7b1313ae507640614aa8fa763b1aed06a92a17
amicha19 <158494961+amicha19@users.noreply.github.com>
2024-02-13 05:56:18 -0500

    Update main.java

--- main.java
+++ main.java
@@ -3,3 +3,27 @@
 // ----------------------------------------------------------
 // This program will convert Fahrenheit to Celsius
 // ----------------------------------------------------------
+
+import java.util.Scanner;
+
+
+public class practicumnum7 {
+    public static void main (String [] args) {
+
+        double fahrenheit;
+        double celsius;
+
+      Scanner user = new Scanner (System.in);
+
+      System.out.println("please enter the temp (in fahrenheit):");
+      fahrenheit = user.nextDouble();
+
+      celsius = celsius_conversion(fahrenheit);
+
+      System.out.printf("%.1f" + "degrees fahrenheit = " + "degrees celsius"}
+
+              public static double celsius_conversion (double convert) {
+                return (convert - 32) * 5 / 9.0;
+              }
+}
+
